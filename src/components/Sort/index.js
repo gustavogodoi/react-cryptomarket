4af5edb0791c196e1925e90d1e4494db3249f778
gustavogodoi <gugodoi@gmail.com>
2018-01-01 16:56:12 +0000

    Including new sort options

--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -8,6 +8,10 @@ const Sort = ({ sort, updateSort }) => (
     <Select id="sort-select" value={sort} onChange={updateSort}>
       <option value="rank">Rank</option>
       <option value="price_eur">Price</option>
+      <option value="24h_volume_eur">Volume 24h</option>
+      <option value="percent_change_1h">Change 1h</option>
+      <option value="percent_change_24h">Change 24h</option>
+      <option value="percent_change_7d">Change 7d</option>
     </Select>
   </SortWrapper>
 );
