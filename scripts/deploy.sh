4f6218547367a2bac90ab826b53b6357cebddfad
Gary Meehan <garymeehan@outlook.com>
2018-01-23 21:03:14 +0000

    remove decryption from deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,6 @@ set -x
 if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_REPO_SLUG = "gustavogodoi/react-cryptomarket" ]
 then
     echo "Starting Deploy"
-    openssl aes-256-cbc -K $encrypted_40d62000dcf5_key -iv $encrypted_40d62000dcf5_iv
-    -in deploy-key.enc -out deploy-key -d
-    rm deploy-key.enc
-    chmod 600 deploy-key
-    mv deploy-key ~/.ssh/id_rsa
     # Initialize a new git repo in _site, and push it to our server.
     cd build
     git init
