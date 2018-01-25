bc699fdd6eb32d2bfd6af13d0ce2fa71c83af555
Gary Meehan <garymeehan@outlook.com>
2018-01-25 20:47:34 +0000

    tweaks

--- server/app.js
+++ server/app.js
@@ -4,22 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var app = express();
 
+var routes = require('./routes/index');
+
+var app = express();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-const root = `${__dirname}../public_html`;
 app.use('/api', routes);
-app.use('/static', express.static(root));
+app.use('/static/', express.static(path.join(__dirname, '../public_html')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(root, 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../public_html', 'index.html'));
 });
 
 // catch 404 and forward to error handler
