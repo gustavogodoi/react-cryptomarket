4aeea7766664f2267315ed491a73836b27eafde1
Gustavo Godoi <gugodoi@gmail.com>
2018-02-20 11:00:04 -0300

    Removing icons alt to prevent layout breaks

--- src/components/CommonFields/CoinIcon/index.js
+++ src/components/CommonFields/CoinIcon/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({ size = '32', symbol = '' }) => (
   <img
-    alt={`Logo for ${symbol}`}
+    alt=""
     src={`https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/${size}/icon/${symbol.toLowerCase()}.png`}
   />
 );
